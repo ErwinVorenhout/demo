ed3f53b9a84e4d9ba57a698dbfadb5dd6525b7d6
Erwin <erwin.vorenhout@hotmail.com>
2020-04-06 17:22:15 +0200

    Styling adjustments
    Styling adjustments

--- index.php
+++ index.php
@@ -8,45 +8,48 @@
 
 </head>
 <body>
-	<form action="api/insert" id= 'registrationForm'>
-		<p id = 'result'></p>
-		<div>
-			<label for="name">Aanhef:</label>
-			<select name="salution">
-				<option value="1">De heer</option>
-				<option value="2">Mevrouw</option>
-				<option value="3">-</option>
-			</select>
-		</div>
-		<div>
-			<label for="firstName">Voornaam:</label>
-			<input type="text" id="firstName" name="firstName" />
-		</div>
-		<div>
-			<label for="prefix">Tussenvoegsel:</label>
-			<input type="text" id="prefix" name="prefix" />
-		</div>
-		<div>
-			<label for="lastName">Achternaam:</label>
-			<input type="text" id="lastName" name="lastName" />
-		</div>
-		<div>
-			<label for="email">E-Mail:</label>
-			<input type="email" id="email" name="email" />
-		</div>
-		<div>
-			<label for="email">Land:</label>
-			  	<input type="text" id="country" />
-				<input type="hidden" id="country_code"  name = 'country_code'/>
 
-				<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-				<script src="src/js/countrySelect.min.js"></script>
-		</div>
-		<div class="button">
-			<button type="submit">Aanmelden</button>
-		</div>
+			
+		<form action="api/insert" id= 'registrationForm'>
+			<p id = 'result'></p>
+			<div>
+				<label for="name">Aanhef:</label>
+				<select name="salution">
+					<option value="1">De heer</option>
+					<option value="2">Mevrouw</option>
+					<option value="3">-</option>
+				</select>
+			</div>
+			<div>
+				<label for="firstName">Voornaam:</label>
+				<input type="text" id="firstName" name="firstName" />
+			</div>
+			<div>
+				<label for="prefix">Tussenvoegsel:</label>
+				<input type="text" id="prefix" name="prefix" />
+			</div>
+			<div>
+				<label for="lastName">Achternaam:</label>
+				<input type="text" id="lastName" name="lastName" />
+			</div>
+			<div>
+				<label for="email">E-Mail:</label>
+				<input type="email" id="email" name="email" />
+			</div>
+			<div>
+				<label for="email">Land:</label>
+				  	<input type="text" id="country" />
+					<input type="hidden" id="country_code"  name = 'country_code'/>
 
-	</form>
+					<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+					<script src="src/js/countrySelect.min.js"></script>
+			</div><br>
+			<div class="button">
+				<button type="submit">Aanmelden</button>
+			</div>
+
+		</form>
+
 
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"
   integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
